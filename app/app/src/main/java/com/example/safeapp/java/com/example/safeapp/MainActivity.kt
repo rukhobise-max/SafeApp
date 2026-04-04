9baa2705bb6a71d64cd194cfe6624d54363bd75b
rukhobise-max <rukhobise@gmail.com>
2026-04-05 02:33:07 +0700

    Update MainActivity.kt
    Signed-off-by: rukhobise-max <rukhobise@gmail.com>

--- app/app/src/main/java/com/example/safeapp/java/com/example/safeapp/MainActivity.kt
+++ app/app/src/main/java/com/example/safeapp/java/com/example/safeapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.safeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
